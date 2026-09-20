c5b3e548620e8e89462dbe0a069fd9d3daaca6f6
suneettipirneni <suneettipirneni@icloud.com>
2026-09-20 11:40:41 -0400

    add speed insights

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@workspace/ui/components/theme-provider"
 import { TooltipProvider } from "@workspace/ui/components/tooltip"
 import "@workspace/ui/globals.css"
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://fortsprite.net"),
@@ -44,6 +44,7 @@ export default function RootLayout({
           <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
